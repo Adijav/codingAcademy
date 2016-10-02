df4d941c23d19180798e1ea970c763d76f123d63
Adijav <agjavali@gmail.com>
2016-10-01 21:02:34 -0700

    updated config.js

--- server/config/config.js
+++ server/config/config.js
@@ -8,9 +8,9 @@
  		port: process.env.PORT || 3000
  	},
  	production: {
- 		db:'',
+ 		db:'mongodb://Adijav:1234567890@ds031845.mlab.com:31845/coding_academy',
  		rootPath: rootPath,
- 		port: process.env.PORT || 5000
+ 		port: process.env.PORT || 80
  	}
 
  };
